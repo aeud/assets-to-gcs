27ac53cc1b34505f39ad3b04da31382f67c7b634
Adrien Eudes <adrien.eudes@gmail.com>
2017-07-05 15:12:11 +0800

    Updates

--- app.js
+++ app.js
@@ -6,35 +6,41 @@ flags.defineString('path', null, 'Path to the asset directory');
 flags.defineString('bucket', null, 'Bucket');
 flags.defineString('keyfile', null, 'Google Key File');
 flags.defineString('project', null, 'Google Project ID');
+flags.defineString('subdir', null, 'Subdirectory to load files to');
 flags.parse();
 
 if (!flags.get('project') || !flags.get('bucket') || !flags.get('keyfile') || !flags.get('project')) {
     throw new Error("Please fill all the parameters");
 }
 
-const gcs = require('@google-cloud/storage')({
-    projectId: flags.get('project'),
-    keyFilename: flags.get('keyfile')
-});
-
-const walk = (origin, p="") => {
+const walk = (origin, subdir, p="") => {
     return fs.readdirSync(`${origin}/${p}`).map(f => {
         let newP = `${p}/${f}`;
         let newPath = `${origin}/${newP}`;
         if (fs.statSync(newPath).isDirectory()) {
-            return walk(origin, newP);
+            return walk(origin, subdir, newP);
         }
         return [{
-            key: newP.substr(1),
+            key: `${subdir ? subdir + "/" : ""}${newP.substr(1)}`,
             path: `${origin}${newP}`
         }];
     }).reduce((a, b) => a.concat(b), []);
 };
 
-const bucket = gcs.bucket('lx-test');
+const gcs = require('@google-cloud/storage')({
+    projectId: flags.get('project'),
+    keyFilename: flags.get('keyfile')
+});
 
-Promise.all(walk(flags.get('path')).map(d => {
-    return bucket.file(d.key).save(fs.readFileSync(d.path, 'utf-8'));
+const bucket = gcs.bucket(flags.get('bucket'));
+
+Promise.all(walk(flags.get('path'), flags.get('subdir')).map(d => {
+    // console.log(d);
+    return new Promise((res, rej) => {
+        fs.createReadStream(d.path)
+        .pipe(bucket.file(d.key).createWriteStream())
+        .on('error', rej)
+        .on('finish', res);
+    });
 }))
-.then(console.log)
 .catch(console.error);
